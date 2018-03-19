8fe7996b19e99c5558957ffe05ca6d33fa719867
claytonknittel <claytonknittel@claytons-mbp.dhcp.wustl.edu>
2018-03-19 15:07:24 -0500

    k

--- steps/steps.ino
+++ steps/steps.ino
@@ -1,11 +1,51 @@
 
 #include <SparkFun_MMA8452Q.h>
 
+MMA8452Q accel;
+
 void setup() {
   Serial.begin(9600);
+  accel.init();
 }
 
 void loop() {
-  
+  if (accel.available()) {
+    accel.read();
+    printA();
+  }
 }
+
+void printA() {
+  Serial.print(cx());
+  Serial.print("\t");
+  Serial.print(cy());
+  Serial.print("\t");
+  Serial.println(cz());
+}
+
+float x() {
+  return accel.x;
+}
+
+float y() {
+  return accel.y;
+}
+
+float z() {
+  return accel.z;
+}
+
+float cx() {
+  return accel.cx;
+}
+
+float cy() {
+  return accel.cy;
+}
+
+float cz() {
+  return accel.cz;
+}
+
+
 
