81ee841fe97bc538adb4f2196fbaa1f5af450eb8
claytonknittel <claytonknittel@claytons-mbp.dhcp.wustl.edu>
2018-03-19 15:49:43 -0500

    k

--- src/studio8/CountSteps.java
+++ src/studio8/CountSteps.java
@@ -1,7 +1,20 @@
 package studio8;
 
-// TODO: Develop an algorithm to count steps in accelerometer data
-//    Major steeps:
-//       1. Create a class and main method.
-//       2. Using a Scanner and File object, read data from your .csv file.
-//       3. Develop and test algorithms to count the "peaks" in the data.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class CountSteps {
+
+	public static void main(String args[]) {
+		File f = new File("");
+		Scanner s;
+		try {
+			s = new Scanner(f);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
--- steps/steps.ino
+++ steps/steps.ino
@@ -3,16 +3,86 @@
 
 MMA8452Q accel;
 
+const int len = 100;
+
+float buf[len]; // buffer
+int loc;        // current location in buffer array
+
+const float threshold = .4;
+const int debounce = 250;
+
+unsigned long delta;
+
+bool isPeaking;   // true if currently in a peak
+
+int numPeaks;
+
+enum State {
+  xd,   // in x-direction
+  yd,   // in y-direction
+  zd    // in z-direction
+};
+
+State state;
+
 void setup() {
   Serial.begin(9600);
   accel.init();
+  loc = 0;
+  isPeaking = false;
+  numPeaks = 0;
+  delta = millis();
 }
 
 void loop() {
   if (accel.available()) {
-    accel.read();
-    printA();
+    readState();
+    updateState();
+    checkPeak();
+    
+    Serial.print(mag());
+    Serial.print(", ");
+//    Serial.print(" ");
+//    Serial.print(numPeaks);
+//    Serial.print(" ");
+//    Serial.println(avg());
   }
+}
+
+
+void checkPeak() {
+  if (mag() - avg() > threshold) {
+    if (!isPeaking && delta < millis()) {
+      isPeaking = true;
+      numPeaks ++;
+      //Serial.println(numPeaks);
+    }
+  } else {
+    if (isPeaking) {
+      delta = millis() + debounce;
+    }
+    isPeaking = false;
+  }
+}
+
+void readState() {
+  accel.read();
+  place(mag());
+}
+
+void place(float num) {
+  buf[(loc++) % len] = num;
+}
+
+float avg() {
+  if (loc == 0) {
+    return 0;
+  }
+  float sum = 0;
+  for (float i : buf) {
+    sum += i;
+  }
+  return sum / min(loc, len);
 }
 
 void printA() {
@@ -21,6 +91,39 @@ void printA() {
   Serial.print(cy());
   Serial.print("\t");
   Serial.println(cz());
+}
+
+void printState() {
+  if (state == xd) {
+    Serial.println("x");
+  } else if (state == yd) {
+    Serial.println("y");
+  } else {
+    Serial.println("z");
+  }
+}
+
+void printMag() {
+  Serial.println(mag());
+}
+
+float mag() {
+  return sqrt(cx() * cx() + cy() * cy() + cz() * cz());
+}
+
+void updateState() {
+  float xx = abs(cx());
+  float yy = abs(cy());
+  float zz = abs(cz());
+  if (xx > yy && xx > zz) {
+    state = xd;
+    return;
+  }
+  if (yy > xx && yy > zz) {
+    state = yd;
+    return;
+  }
+  state = zd;
 }
 
 float x() {
